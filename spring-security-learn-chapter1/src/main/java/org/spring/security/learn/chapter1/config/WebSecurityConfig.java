f4995a2b7b8165dec5c11c69af2b2e902f0bf1d1
chaozhang17 <chaozhang17@iflytek.com>
2019-09-23 22:09:12 +0800

    add csrf branch

--- spring-security-learn-chapter1/src/main/java/org/spring/security/learn/chapter1/config/WebSecurityConfig.java
+++ spring-security-learn-chapter1/src/main/java/org/spring/security/learn/chapter1/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package org.spring.security.learn.chapter1.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,13 +15,17 @@ import javax.servlet.http.HttpServletRequest;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Value("${spring.csrf.enabled}")
+	private boolean enabled;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests().antMatchers("/anon1", "/anon2").permitAll().anyRequest().authenticated().and()
 				.formLogin().loginPage("/login").defaultSuccessUrl("/user").permitAll().and().logout().permitAll();
-
+		if(!enabled) {
+			http.csrf().disable();
+		}
 //		RequestMatcher requestMatcher = new RequestMatcher(){
 //
 //			/**
--- spring-security-learn-chapter1/src/main/java/org/spring/security/learn/chapter1/controller/TestController.java
+++ spring-security-learn-chapter1/src/main/java/org/spring/security/learn/chapter1/controller/TestController.java
@@ -1,36 +1,47 @@
 package org.spring.security.learn.chapter1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
-	
-	@RequestMapping({"/anon1","/anon2"})
-	public String anon() {
-		return "anon";
-	}
-	
-	@RequestMapping("/login")
-	public String login() {
-		return "login";
-	}
-	
-	@RequestMapping("/")
-	public String index() {
-		return "index";
-	}
-	
-	@RequestMapping("/user")
-	public String user() {
-		return "user";
-	}
-	@PostMapping("/user1")
-	@ResponseBody
-	public String user1() {
-		return "user1";
-	}
 
+    @RequestMapping({"/anon1", "/anon2"})
+    public String anon() {
+        return "anon";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @PostMapping("/user1")
+    @ResponseBody
+    public String user1() {
+        return "user1";
+    }
+
+
+    @RequestMapping("/clickMe")
+    public String clickMe(@RequestParam(required = false) String amount) {
+        if (!StringUtils.isEmpty(amount)) {
+            System.out.println("evil user steal money now");
+        }
+        return "clickMe";
+    }
 }
