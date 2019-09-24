f96defc4bf726b6f402a70f4413b43a613ac193a
chaozhang17 <chaozhang17@iflytek.com>
2019-09-24 11:25:19 +0800

    CookieCsrfTokenRepository config

--- spring-security-learn-chapter1/src/main/java/org/spring/security/learn/chapter1/config/WebSecurityConfig.java
+++ spring-security-learn-chapter1/src/main/java/org/spring/security/learn/chapter1/config/WebSecurityConfig.java
@@ -8,10 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.util.StringUtils;
-
-import javax.servlet.http.HttpServletRequest;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,36 +21,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.formLogin().loginPage("/login").defaultSuccessUrl("/user").permitAll().and().logout().permitAll();
 		if(!enabled) {
 			http.csrf().disable();
+		} else {
+			CookieCsrfTokenRepository cookieCsrfTokenRepository =   new CookieCsrfTokenRepository();
+			cookieCsrfTokenRepository.setCookieHttpOnly(false);
+			http.csrf().csrfTokenRepository(cookieCsrfTokenRepository);
 		}
-//		RequestMatcher requestMatcher = new RequestMatcher(){
-//
-//			/**
-//			 * http://127.0.0.1:9090/user?token=abc 可以访问
-//			 * http://127.0.0.1:9090/user  不能够访问
-//			 * @param request
-//			 * @return
-//			 */
-//			@Override
-//			public boolean matches(HttpServletRequest request) {
-//				if(!request.getRequestURI().contains("user1")){
-//					return false;
-//				}
-//				/**
-//				 * 拦截命令行伪造的请求
-//				 */
-//				if(StringUtils.isEmpty(request.getParameter("token"))){
-//					return true;
-//				}
-//				if(request.getParameter("token").contains("/login")){
-//					System.out.println("test success");
-//					return false;
-//				}
-//				return false;
-//			}
-//		};
-//		http.csrf().csrfTokenRepository(new CookieCsrfTokenRepository())
-//				.requireCsrfProtectionMatcher(requestMatcher);
-
 	}
 
 
